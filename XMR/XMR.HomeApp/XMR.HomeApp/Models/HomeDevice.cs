4685e69ad00b61b9f30a2c9fb682ff3221b5435a
valmon55 <fedor_ka@mail.ru>
2024-03-10 11:18:06 +0300

    + INotifyPropertyChanged отображается изменение

--- XMR/XMR.HomeApp/XMR.HomeApp/Models/HomeDevice.cs
+++ XMR/XMR.HomeApp/XMR.HomeApp/Models/HomeDevice.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace XMR.HomeApp.Models
 {
-    public class HomeDevice
+    public class HomeDevice : INotifyPropertyChanged
     {
+        private string _name;
+        private string _description;
+
+        /// <summary>
+        /// Делегат, указывающий на метод-обработчик события PropertyChanged, возникающего при изменении свойств компонента
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Метод, вызывающий событие PropertyChanged
+        /// </summary>
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string Name 
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value) 
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        // Обновления этого свойства теперь получают все страинцы
+        public string Description
+        {
+            get { return _description; }
+
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    // Вызов уведомления при изменении
+                    OnPropertyChanged("Description");
+                }
+            }
+        }
         public string Image { get; set; }
-        public string Description { get; set; }
         public string Room { get; set; }
         public HomeDevice(string name = null, string image = null, string description = null, string room = null)
         {
