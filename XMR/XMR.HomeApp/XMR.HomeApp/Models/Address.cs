e762398cd13d4a42766eedea5de0d91d097d14be
valmon55 <fedor_ka@mail.ru>
2024-03-13 18:42:35 +0300

    модели дома для HomeApp

---
+++ XMR/XMR.HomeApp/XMR.HomeApp/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XMR.HomeApp.Models
+{
+    public class Address
+    {
+        public int House { get; set; }
+        public int Building { get; set; }
+        public string Street { get; set; }
+    }
+}
---
+++ XMR/XMR.HomeApp/XMR.HomeApp/Models/HouseInfo.cs
@@ -0,0 +1,19 @@
+﻿using HomeApi.Contracts.Models.Home;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XMR.HomeApp.Models
+{
+    public class HouseInfo
+    {
+        public int FloorAmount { get; set; }
+        public string Telephone { get; set; }
+        public string Heating { get; set; }
+        public int CurrentVolts { get; set; }
+        public bool GasConnected { get; set; }
+        public int Area { get; set; }
+        public string Material { get; set; }
+        public Address Address { get; set; }
+    }
+}
